448d4d5b7e256d5642f7a2882fb3c3de6484777f
Marcos Rafael Lapa de Sousa <marcosrafaellsousa@gmail.com>
2019-05-13 16:30:13 -0300

    Consulta simples com query methods

--- src/main/java/com/github/marcosrafaellsousa/workshopmongo/services/PostService.java
+++ src/main/java/com/github/marcosrafaellsousa/workshopmongo/services/PostService.java
@@ -6,6 +6,7 @@ import com.github.marcosrafaellsousa.workshopmongo.services.exception.ObjectNotF
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,5 +19,9 @@ public class PostService {
     public Post findById(String id) {
         Optional<Post> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return repo.findByTitleContainingIgnoreCase(text);
     }
 }
